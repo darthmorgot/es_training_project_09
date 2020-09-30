2ed25b9b106f2a570b61c42bfa1b486abf577cf0
darthmorgot <dartmorgot@yandex.ru>
2020-09-30 16:28:35 +1000

    Обновил код файла

--- src/presenter/task.js
+++ src/presenter/task.js
@@ -2,6 +2,7 @@ import TaskView from '../view/task';
 import TaskEditView from '../view/task-edit';
 import {render, replace, remove} from '../utils/render';
 import {UserAction, UpdateType} from '../const';
+import {isTaskRepeating, isDatesEqual} from '../utils/task';
 
 const Mode = {
   DEFAULT: `DEFAULT`,
@@ -22,6 +23,7 @@ export default class Task {
     this._handleArchiveClick = this._handleArchiveClick.bind(this);
     this._handleFavoriteClick = this._handleFavoriteClick.bind(this);
     this._handleFormSubmit = this._handleFormSubmit.bind(this);
+    this._handleDeleteClick = this._handleDeleteClick.bind(this);
     this._escKeyDownHandler = this._escKeyDownHandler.bind(this);
   }
 
@@ -38,6 +40,7 @@ export default class Task {
     this._taskComponent.setArchiveClickHandler(this._handleArchiveClick);
     this._taskComponent.setFavoriteClickHandler(this._handleFavoriteClick);
     this._taskEditComponent.setFormSubmitHandler(this._handleFormSubmit);
+    this._taskEditComponent.setDeleteClickHandler(this._handleDeleteClick);
 
     if (prevTaskComponent === null || prevTaskEditComponent === null) {
       render(this._taskListContainer, this._taskComponent);
@@ -106,11 +109,22 @@ export default class Task {
         Object.assign({}, this._task, {isArchive: !this._task.isArchive}));
   }
 
-  _handleFormSubmit(task) {
+  _handleFormSubmit(update) {
+    const isMinorUpdate =
+      !isDatesEqual(this._task.dueDate, update.dueDate) ||
+      isTaskRepeating(this._task.repeating) !== isTaskRepeating(update.repeating);
+
+    this._changeData(
+        UserAction.UPDATE_TASK,
+        isMinorUpdate ? UpdateType.MINOR : UpdateType.PATCH,
+        update);
+    this._replaceFormToCard();
+  }
+
+  _handleDeleteClick(task) {
     this._changeData(
         UserAction.UPDATE_TASK,
         UpdateType.MINOR,
         task);
-    this._replaceFormToCard();
   }
 }
