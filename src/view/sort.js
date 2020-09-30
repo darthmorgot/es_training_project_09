68720b8901255eecb4df52ae2969c70c651c4a9f
darthmorgot <dartmorgot@yandex.ru>
2020-09-30 15:50:02 +1000

    Обновил код файла

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,11 +1,20 @@
 import AbstractView from './abstract';
 import {SortType} from '../const';
 
-const createSortTemplate = () => {
+const createSortTemplate = (currentSortType) => {
   return `<div class="board__filter-list">
-      <a href="#" class="board__filter" data-sort-type="${SortType.DEFAULT}">SORT BY DEFAULT</a>
-      <a href="#" class="board__filter" data-sort-type="${SortType.DATE_UP}">SORT BY DATE up</a>
-      <a href="#" class="board__filter" data-sort-type="${SortType.DATE_DOWN}">SORT BY DATE down</a>
+      <a href="#"
+        class="board__filter ${currentSortType === SortType.DEFAULT ? `board__filter--active` : ``}"
+        data-sort-type="${SortType.DEFAULT}"
+      >SORT BY DEFAULT</a>
+      <a href="#"
+        class="board__filter ${currentSortType === SortType.DATE_UP ? `board__filter--active` : ``}"
+        data-sort-type="${SortType.DATE_UP}"
+      >SORT BY DATE up</a>
+      <a href="#"
+        class="board__filter ${currentSortType === SortType.DATE_DOWN ? `board__filter--active` : ``}"
+        data-sort-type="${SortType.DATE_DOWN}"
+      >SORT BY DATE down</a>
     </div>`;
 };
 
