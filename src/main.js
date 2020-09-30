bf97640d206eb8cbb9028c6aa77f498ef9d304ab
darthmorgot <dartmorgot@yandex.ru>
2020-09-30 14:10:36 +1000

    Обновил код файла

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import SiteMenuView from './view/site-menu';
 import FilterView from './view/filter';
 import BoardPresenter from './presenter/board';
+import TasksModel from './model/tasks';
 import {generateTask} from './mock/task';
 import {generateFilter} from './mock/filter';
 import {render} from './utils/render';
@@ -10,10 +11,13 @@ const TASK_COUNT = 22;
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
 
+const tasksModel = new TasksModel();
+tasksModel.setTask(tasks);
+
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(siteMainElement);
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
 render(siteHeaderElement, new SiteMenuView());
 render(siteMainElement, new FilterView(filters));
